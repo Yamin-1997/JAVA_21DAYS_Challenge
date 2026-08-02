da68bf85b59b89a542e90991e9711fa8359d0d04
YAMIN EI <yaminei67611@gmail.com>
2026-08-03 02:26:45 +0900

    Day 1 - Variables

--- src/Variables.java
+++ src/Variables.java
@@ -2,7 +2,38 @@
 //Date=2026.08.3
 //Variables
 public class Variables {
+     public static void main(String[]args){
+         System.out.println("Data Types");
+         System.out.println("___________");
 
+         System.out.println("boolean");
+         boolean isloggedIn=true;
+         System.out.println("User is logges in?"+isloggedIn);
+         System.out.println("___________");
+
+         System.out.println("byte");
+         byte age=29;
+         System.out.println("My age is will be  "+age+"in this year of August");
+         System.out.println("___________");
+
+         System.out.println("short about 32000");
+         short price =30000;
+         System.out.println("ths outfit of price is "+price+"円");
+         System.out.println("___________");
+
+         System.out.println("integer");
+         int amount =150000;
+         System.out.println("amount is "+amount);
+
+         long BankAccount=1240133645L;
+         System.out.println("my Bankaccount is "+BankAccount);
+
+         String PhoneNumber="08064492342";
+         System.out.println("my phone number is "+PhoneNumber);
+
+         char grade='A';
+         System.out.println("Grate is "+grade);
+     }
 
 
 }
